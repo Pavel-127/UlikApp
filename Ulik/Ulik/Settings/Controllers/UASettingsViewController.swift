1f56c950819bec3c8c75470960d11dcc6ef129f9
Pavel Kuzmich <pavel_127@mail.ru>
2021-04-05 00:29:10 +0300

    ADDED SnapKit

---
+++ Ulik/Ulik/Settings/Controllers/UASettingsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  UASettingsViewController.swift
+//  Ulik
+//
+//  Created by macbook on 4/4/21.
+//
+
+import UIKit
+
+class UASettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
--- Ulik/Ulik/UAInitViewController.swift
+++ Ulik/Ulik/UAInitViewController.swift
@@ -9,18 +9,30 @@ import UIKit
 
 class UAInitViewController: UIViewController {
 
-    private lazy var backgroundImageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.image = UIImage(named: "splashImage")
-        imageView.contentMode = .center
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-
-        return imageView
-    }()
+//    private lazy var backgroundImageView: UIImageView = {
+//        let imageView = UIImageView()
+//        imageView.image = UIImage(named: "background")
+//        imageView.contentMode = .scaleAspectFill
+//        imageView.translatesAutoresizingMaskIntoConstraints = false
+//
+//        return imageView
+//    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.view.addSubview(backgroundImageView)
+//        self.view.addSubview(backgroundImageView)
+    }
+
+    @objc private func continueScree() {
+        let tabBarController = UITabBarController()
+        tabBarController.setViewControllers([
+        UAMainViewController(),
+        UAStatisticsViewController(),
+        UAListViewController(),
+        UASettingsViewController()],
+        animated: true)
+
+        (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.changeRootViewController(tabBarController)
     }
 }
