a6154d279199be79be4a7c3876006dbfbf6263e5
Pavel Kuzmich <pavel_127@mail.ru>
2021-04-11 23:11:03 +0300

    ADDED background image

--- Ulik/Ulik/Statistics/Controllers/UAStatisticsViewController.swift
+++ Ulik/Ulik/Statistics/Controllers/UAStatisticsViewController.swift
@@ -9,9 +9,26 @@ import UIKit
 
 class UAStatisticsViewController: UIViewController {
 
+    private lazy var backgroundImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "background")
+        imageView.contentMode = .scaleAspectFill
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.alpha = 0.5
+
+        return imageView
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = "Статистика"
 
+        self.view.backgroundColor = .white
+
+        self.view.addSubview(backgroundImageView)
+
+        self.backgroundImageView.snp.makeConstraints { (make) in
+            make.edges.equalToSuperview()
+        }
     }
-
 }
