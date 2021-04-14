ff0451ef4b46329d61175f2d87e9e260bd208125
Pavel Kuzmich <pavel_127@mail.ru>
2021-04-14 19:41:03 +0300

    ADDED Cell

---
+++ Ulik/Ulik/Main/Views/Cells/UAMainCell.swift
@@ -0,0 +1,19 @@
+//
+//  UAMainCell.swift
+//  Ulik
+//
+//  Created by macbook on 4/14/21.
+//
+
+import UIKit
+
+class UAMainCell: UITableViewCell {
+
+
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+
+}
