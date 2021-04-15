895c8bfa7145812918498a40c8ed78ac6452ebb2
Pavel Kuzmich <pavel_127@mail.ru>
2021-04-15 16:15:38 +0300

    ADDED Model UATransport

---
+++ Ulik/Ulik/Features/Main/Models/UATransport.swift
@@ -0,0 +1,16 @@
+//
+//  UATransport.swift
+//  Ulik
+//
+//  Created by macbook on 4/15/21.
+//
+
+import Foundation
+import UIKit
+
+struct UATransport {
+    let id = UUID()
+    let image: UIImage
+    let title: String
+    var description: String
+}
